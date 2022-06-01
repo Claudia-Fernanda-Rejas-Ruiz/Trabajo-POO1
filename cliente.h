66901069dc9bec86d337f348bd321051a01d12f9
Claudia Fernanda Rejas Ruiz <83561363+Claudia-Fernanda-Rejas-Ruiz@users.noreply.github.com>
2022-06-01 01:01:31 -0500

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,1 +1,45 @@
+#include<string>
+using namespace std;
+class Cliente
+{
+private:
+	int 	orden;
+  int codigo; 
+	string	nombre;
 
+public:
+	Cliente()
+	{
+
+	}
+
+	~Cliente()
+	{
+
+	}
+	void	setOrden(int ord)
+	{
+		orden = ord;
+	}
+  void	setCodigo(int cod)
+	{
+		codigo = cod;
+	}
+	void	setNombre(string nom)
+	{
+		nombre = nom;
+	}
+
+	int 	getOrden()
+	{
+		return this->orden;
+	}
+  int 	getCodigo()
+	{
+		return this->codigo;
+	}
+	string 	getNombre()
+	{
+		return this->nombre;
+	}
+};
