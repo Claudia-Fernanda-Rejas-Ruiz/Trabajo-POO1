208e642891e747b3b601730db6fa46b9fbfc05e4
DanielQuispe1X2 <85980908+DanielQuispe1X2@users.noreply.github.com>
2022-06-01 20:02:17 -0500

    Update vendedor.h

--- vendedor.h
+++ vendedor.h
@@ -1,3 +1,27 @@
-//Hoasfdaf
-//Hola
-//hhhf
+#include <string>
+using namespace std;
+class Vendedor{
+private:
+  string usuario;
+  int contraseña;
+public:
+  Vendedor(){
+    
+  }
+  ~Vendedor(){
+    
+  }
+  void setUsuario(string user){
+    usuario=user;
+  }
+  void setcontraseña(int contraseña){
+    usuario=contraseña;
+  }
+  string getUsuario(){
+    return this->usuario;
+  }
+  int getContraseña(){
+    return this->contraseña;
+  }
+
+};
